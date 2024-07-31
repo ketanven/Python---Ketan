404a607447c7acf6c5d35b4ca6566c5470899fd6
Ketan Venkatesh Pillai <98447291+ketanven@users.noreply.github.com>
2024-07-31 13:12:24 +0530

    Python

---
+++ ASSINGEMNT/Python - Rest Framework - Industry Program/myproject/books/views.py
@@ -0,0 +1,13 @@
+# books/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
